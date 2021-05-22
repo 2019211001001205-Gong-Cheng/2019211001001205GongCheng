5f14062bde3a5f1a659368a0f7539c3ac0e58984
2019211001001205GongCheng <1078601907@qq.com>
2021-05-22 20:31:48 +0800

    HW-Week12-Commit

--- src/main/java/com/gongcheng/controller/GetImgServlet.java
+++ src/main/java/com/gongcheng/controller/GetImgServlet.java
@@ -1,37 +1,43 @@
 package com.gongcheng.controller;
 
 import com.gongcheng.dao.ProductDao;
-import com.gongcheng.model.Product;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/productList")
-public class ProductListServlet extends HttpServlet {
-    Connection con = null;
-
+@WebServlet(name = "GetImgServlet", value = "/getImg")//url
+public class GetImgServlet extends HttpServlet {
+    Connection con=null;
     @Override
     public void init() throws ServletException{
         super.init();
         con=(Connection) getServletContext().getAttribute("con");
     }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao = new ProductDao();
+
+        int id=0;
+        if (request.getParameter("id")!=null)
+             id = Integer.parseInt(request.getParameter("id"));
+        ProductDao productDao=new ProductDao();
+        byte[] imgByte=new byte[0];
         try {
-            List<Product> productList=productDao.findAll(con);
-            request.setAttribute("productList",productList);
+            imgByte=productDao.getPictureById(id,con);
+            if (imgByte!=null){
+                response.setContentType("image/gif");
+                OutputStream out = response.getOutputStream();
+                out.write(imgByte);
+                out.flush();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        String path="../WEB-INF/views/admin/productList.jsp";
-        request.getRequestDispatcher(path).forward(request,response);
+
     }
 
     @Override
--- src/main/java/com/gongcheng/controller/ProductDetailsServlet.java
+++ src/main/java/com/gongcheng/controller/ProductDetailsServlet.java
@@ -1,6 +1,7 @@
 package com.gongcheng.controller;
 
 import com.gongcheng.dao.ProductDao;
+import com.gongcheng.model.Category;
 import com.gongcheng.model.Product;
 
 import javax.servlet.*;
@@ -11,28 +12,38 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/productList")
-public class ProductListServlet extends HttpServlet {
+@WebServlet(name = "ProductDetailsServlet", value = "/productDetails")
+public class ProductDetailsServlet extends HttpServlet {
     Connection con = null;
-
     @Override
-    public void init() throws ServletException{
+    public void init() throws ServletException {
         super.init();
         con=(Connection) getServletContext().getAttribute("con");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao = new ProductDao();
+
         try {
-            List<Product> productList=productDao.findAll(con);
-            request.setAttribute("productList",productList);
+            List<Category> categoryList=Category.findAllCategory(con);
+            request.setAttribute("categoryList",categoryList);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        String path="../WEB-INF/views/admin/productList.jsp";
+        try {
+        if (request.getParameter("id")!=null) {
+            int productId = Integer.parseInt(request.getParameter("id"));
+            ProductDao productDao = new ProductDao();
+            Product product = productDao.findById(productId, con);
+            request.setAttribute("p",product);
+        }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        String path="/WEB-INF/views/productDetails.jsp";
         request.getRequestDispatcher(path).forward(request,response);
     }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
